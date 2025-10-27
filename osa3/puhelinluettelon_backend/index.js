29b33f038b1dbc6a5f5a29aefadfb6186fe2903e
Teemu Kettukangas <teemu.kettukangas@gmail.com>
2025-10-27 17:49:32 +0200

    ensimmäinen production build

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -11,6 +11,7 @@ morgan.token('data', function getData (req) {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
